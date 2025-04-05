dad35517ff60b5c998c7eef041bbc77dd47f4624
Zhiyuan Liu <thenukezealot@gmail.com>
2025-04-05 17:27:25 +0300

    tab bar design

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,47 @@
+import { AntDesign } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { useColorScheme } from "react-native";
+import { Colors } from "@/constants/Colors";
 
 export default function TabsLayout() {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme ?? "light"];
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: theme.primary,
+        tabBarStyle: {
+          height: 65,
+          paddingTop: 10,
+          paddingBottom: 5,
+        }
       }}
     >
-      <Tabs.Screen name="chat" />
-      <Tabs.Screen name="profile" />
+      <Tabs.Screen name="chat"
+        options={{
+          title: "Chat",
+          tabBarIcon: ({ color }) => (
+            <AntDesign
+              name="message1"
+              size={22}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Tabs.Screen name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <AntDesign
+              name="user"
+              size={22}
+              color={color}
+            />
+          )
+        }}
+      />
     </Tabs>
   );
 }
